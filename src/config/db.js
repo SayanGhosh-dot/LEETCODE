13333d6fabb5823c7703cc789986233d0c2c5ac1
Sayan Ghosh <sayanghoshxyz05@gmail.com.com>
2026-01-28 23:43:32 +0530

    fourth commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,13 @@
 const mongoose=require("mongoose");
 async function main()
 {
-    console.log("Connecting to MongoDB...");
-    console.log("DB_CONNECT_KEY:", process.env.DB_CONNECT_KEY ? "Set" : "Not set");
     
     if (!process.env.DB_CONNECT_KEY) {
         throw new Error("DB_CONNECT_KEY is not defined in .env file");
     }
     
     await mongoose.connect(process.env.DB_CONNECT_KEY);
-    console.log("MongoDB connected successfully");
+    // console.log("MongoDB connected successfully");
     return mongoose.connection;
 }
 module.exports=main;
